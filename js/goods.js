71f351bd904e0162887d1a01a0eeb865d3f015c8
foremna <missmoxi2011@yandex.ru>
2018-09-21 17:57:51 +0300

    правка

--- js/goods.js
+++ js/goods.js
@@ -2,7 +2,7 @@
 
 var tastes = ['Чесночные сливки', 'Огуречный педант', 'Молочная хрюша', 'Грибной шейк', 'Баклажановое безумие', 'Паприколу итальяно', 'Нинзя-удар васаби', 'Хитрый баклажан', 'Горчичный вызов', 'Кедровая липучка', 'Корманный портвейн', 'Чилийский задира', 'Беконовый взрыв', 'Арахис vs виноград', 'Сельдерейная душа', 'Початок в бутылке', 'Чернющий мистер чеснок', 'Раша федераша', 'Кислая мина', 'Кукурузное утро', 'Икорный фуршет', 'Новогоднее настроение', 'С пивком потянет', 'Мисс креветка', 'Бесконечный взрыв', 'Невинные винные', 'Бельгийское пенное', 'Острый язычок'];
 
-var pictures = ['gum-cedar', 'gum-chile', 'gum-eggplant', 'gum-mustard', 'gum-portwine', 'gum-wasabi', 'ice-cucumber', 'ice-eggplant', 'ice-garlic', 'ice-italian', 'ice-mushroom', 'marmalade-beer', 'marmalade-caviar', 'marmalade-corn', 'marmalade-new-year', 'marmalade-sour', 'marshmallow-bacon', 'marshmallow-beer', 'marshmallow-shrimp', 'marshmallow-spicg', 'marshmallow-wine', 'soda-bacon', 'soda-celery', 'soda-cob', 'soda-garlic', 'soda-peanut-grapes', 'soda-russian'];
+var pictures = ['gum-cedar', 'gum-chile', 'gum-eggplant', 'gum-mustard', 'gum-portwine', 'gum-wasabi', 'ice-cucumber', 'ice-eggplant', 'ice-garlic', 'ice-italian', 'ice-mushroom', 'marmalade-beer', 'marmalade-caviar', 'marmalade-corn', 'marmalade-new-year', 'marmalade-sour', 'marshmallow-bacon', 'marshmallow-beer', 'marshmallow-shrimp', 'marshmallow-spicy', 'marshmallow-wine', 'soda-bacon', 'soda-celery', 'soda-cob', 'soda-garlic', 'soda-peanut-grapes', 'soda-russian'];
 
 var ingridients = ['молоко', 'сливки', 'вода', 'пищевой краситель', 'патока', 'ароматизатор бекона', 'ароматизатор свинца', 'ароматизатор дуба, идентичный натуральному', 'ароматизатор картофеля', 'лимонная кислота', 'загуститель', 'эмульгатор', 'консервант: сорбат калия', 'посолочная смесь: соль нитрит натрия', 'ксилит', 'карбамид', 'вилларибо', 'виллабаджо'];
 
@@ -20,8 +20,6 @@ var getRandomBool = function (bool) {
   }
 };
 
-var gustos = [];
-
 var shuffleArray = function (array) {
   array = array.slice();
 
@@ -34,19 +32,20 @@ var shuffleArray = function (array) {
   return array;
 };
 
-var randomTastesy = function (array) {
+var getRandomTastes = function (array) {
   array = shuffleArray(array);
 
-  var randomLength = getRandomInRange(0, array.length);
+  var randomLength = getRandomInRange(1, array.length);
 
-  return array.slice(0, randomLength).join(', ');
+  return array.slice(1, randomLength).join(', ');
 };
 
-var choiseTastes = function () {
+var getObject = function () {
+  var gustos = [];
   for (var i = 0; i < 26; i++) {
-    var choises = {
-      name: tastes[i],
-      picture: pictures[i],
+    gustos[i] = {
+      name: tastes[getRandomInRange(1, tastes.length - 1)],
+      picture: pictures[getRandomInRange(1, pictures.length - 1)],
       amount: getRandomInRange(0, 20),
       price: getRandomInRange(100, 1500),
       weight: getRandomInRange(30, 300),
@@ -57,20 +56,24 @@ var choiseTastes = function () {
       nutritionFacts: {
         sugar: getRandomBool(),
         energy: getRandomInRange(70, 500),
-        contents: randomTastesy(ingridients)
+        contents: getRandomTastes(ingridients)
       }
     };
-    gustos.push(choises);
   }
+  return gustos;
 };
 
-choiseTastes();
+var gustos = getObject();
 
-var cardsAddLoad = document.querySelector('.catalog__cards');
-cardsAddLoad.classList.remove('catalog__cards--load');
+var cardsHiddenAndAdd = function () {
+  var cardsAddLoad = document.querySelector('.catalog__cards');
+  cardsAddLoad.classList.remove('catalog__cards--load');
 
-var cardsHiddenLoad = document.querySelector('.catalog__load');
-cardsHiddenLoad.classList.add('visually-hidden');
+  var cardsHiddenLoad = document.querySelector('.catalog__load');
+  cardsHiddenLoad.classList.add('visually-hidden');
+};
+
+cardsHiddenAndAdd();
 
 var cardTemplate = document.querySelector('#card')
     .content
@@ -83,11 +86,9 @@ var createCard = function (templateNode, dataArray, parent, className) {
 
   if (dataArray[i].amount > 5) {
     cardClone.classList.add('card--in-stock');
-  } else if (dataArray[i].amount <= 5 && dataArray[i].amount >= 1) {
-    cardClone.classList.remove('card--in-stock');
+  } else if (dataArray[i].amount >= 1) {
     cardClone.classList.add('card--little');
   } else if (dataArray[i].amount === 0) {
-    cardClone.classList.remove('card--in-stock');
     cardClone.classList.add('card--soon');
   }
 
@@ -103,18 +104,21 @@ var createCard = function (templateNode, dataArray, parent, className) {
   if (cardClone.querySelector('.stars__rating')) {
     var starsRating = cardClone.querySelector('.stars__rating');
 
-    if (dataArray[i].rating.value === 4) {
-      starsRating.classList.remove('stars__rating--five');
-      starsRating.classList.add('stars__rating--four');
-    } else if (dataArray[i].rating.value === 3) {
-      starsRating.classList.remove('stars__rating--five');
-      starsRating.classList.add('stars__rating--three');
-    } else if (dataArray[i].rating.value === 2) {
-      starsRating.classList.remove('stars__rating--five');
-      starsRating.classList.add('stars__rating--two');
-    } else if (dataArray[i].rating.value === 1) {
-      starsRating.classList.remove('stars__rating--five');
-      starsRating.classList.add('stars__rating--one');
+    switch (dataArray[i].rating.value) {
+      case 4:
+        starsRating.classList.add('stars__rating--four');
+        break;
+      case 3:
+        starsRating.classList.add('stars__rating--three');
+        break;
+      case 2:
+        starsRating.classList.add('stars__rating--two');
+        break;
+      case 1:
+        starsRating.classList.add('stars__rating--one');
+        break;
+      default:
+        starsRating.classList.add('stars__rating--five');
     }
   }
 
@@ -127,11 +131,7 @@ var createCard = function (templateNode, dataArray, parent, className) {
   if (cardClone.querySelector('.card__characteristic')) {
     var cardCharacteristic = cardClone.querySelector('.card__characteristic');
 
-    if (dataArray[i].nutritionFacts.sugar >= 0.5) {
-      cardCharacteristic.innerHTML = '<p>Содержит сахар</p>';
-    } else {
-      cardCharacteristic.innerHTML = '<p>Без сахара</p>';
-    }
+    cardCharacteristic.innerHTML = (dataArray[i].nutritionFacts.sugar >= 0.5) ? '<p>Содержит сахар</p>' : '<p>Без сахара</p>';
   }
 
   if (cardClone.querySelector('.card__composition-list')) {
@@ -157,10 +157,14 @@ for (i = 0; i < 3; i++) {
   createCard(cardOrderGoods, gustos, basket, 'card-order');
 }
 
-var goodCardRemove = document.querySelector('.goods__cards');
+var goodCardRemovesAndAdd = function () {
+  var goodCardRemove = document.querySelector('.goods__cards');
 
-goodCardRemove.classList.remove('goods__cards--empty');
+  goodCardRemove.classList.remove('goods__cards--empty');
 
-var goodEmptyAdd = document.querySelector('.goods__card-empty');
+  var goodEmptyAdd = document.querySelector('.goods__card-empty');
 
-goodEmptyAdd.classList.add('visually-hidden');
+  goodEmptyAdd.classList.add('visually-hidden');
+};
+
+goodCardRemovesAndAdd();
